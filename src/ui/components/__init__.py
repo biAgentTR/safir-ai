5c12d03141e7a644f8ae2da608f4ecc62e2726db
Esra Ersoy <ersoyesra2002@gmail.com>
2026-08-17 00:47:27 +0300

    feat(ui): add history_view, evidence_gallery and risk_banner components

---
+++ src/ui/components/__init__.py
@@ -0,0 +1,5 @@
+from .input_panel import *
+from .live_progress import *
+from .history_view import HistoryViewComponent
+from .evidence_gallery import EvidenceGalleryComponent
+from .risk_banner import RiskBannerComponent
